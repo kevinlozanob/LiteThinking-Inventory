6eaa3508fef2748d3c40ca94071166c7af6f3d10
kevinlozanob <kevin.lozano-b@uniminuto.edu.co>
2026-01-10 21:22:19 -0500

    feat(backend): implementar generacion de PDF de inventario de productos

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import EmpresaModel, ProductoModel
 
-# Register your models here.
+@admin.register(EmpresaModel)
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('nit', 'nombre', 'telefono')
+    search_fields = ('nombre', 'nit')
+
+@admin.register(ProductoModel)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'empresa')
+    list_filter = ('empresa',)
+    search_fields = ('nombre', 'codigo')
--- backend/core/views.py
+++ backend/core/views.py
@@ -2,6 +2,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from .models import EmpresaModel, ProductoModel
 from .serializers import EmpresaSerializer, ProductoSerializer
+from django.http import FileResponse # <--- Importar
+from rest_framework.decorators import action # <--- Importar
+from rest_framework.response import Response # <--- Importar
+from .utils import generar_pdf_inventario # <--- Importar
 
 class EmpresaViewSet(viewsets.ModelViewSet):
     queryset = EmpresaModel.objects.all()
@@ -13,3 +17,17 @@ class ProductoViewSet(viewsets.ModelViewSet):
     queryset = ProductoModel.objects.all()
     serializer_class = ProductoSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    # GET /api/productos/descargar_reporte/
+    @action(detail=False, methods=['get'])
+    def descargar_reporte(self, request):
+        """
+        Genera y descarga un PDF con el inventario actual.
+        """
+        productos = self.get_queryset()
+        buffer = generar_pdf_inventario(productos)
+        
+        return FileResponse(
+            buffer, 
+            as_attachment=True, 
+            filename='inventario_productos.pdf'
+        )
